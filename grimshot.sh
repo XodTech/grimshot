6d40baa7027f80e629b24de21caabc2c4ac6b3ed
XodTech <82662318+XodTech@users.noreply.github.com>
2025-04-16 18:55:31 +0200

    v2.0 Improved code structure,implemented screenshot name selection and copy only mode

--- grimshot.sh
+++ grimshot.sh
@@ -1,10 +1,40 @@
 #!/bin/sh
 
-directory="/home/user/Images/Screenshots/Grimshot"
-name=$(date +"%d-%m-%Y_%H:%M").png
-output="$directory/$name"
 
-grim $output
-wl-copy < $output
+default_extention="png"
+directory="$HOME/Images/Screenshots/Grimshot"
 
-notify-send "Grimshot" "Screenshot <i>$name</i> has been made"
+function copy_only(){
+
+    name="tmp.png"
+    output="$directory/$name"
+    
+    grim $output
+    wl-copy < $output
+    rm $output
+
+    notify-send "Grimshot" "Screenshot <i>$name</i> has been made using:\n<b>Copy only mode</b>"
+    exit
+}
+
+function normalScreenshot() {
+    if [ "$name" == ".$default_extention" ]; then
+        exit
+    fi
+    output="$directory/$name"
+    
+    grim $output
+    wl-copy < $output
+
+    notify-send "Grimshot" "Screenshot <i>$name</i> has been made"
+}
+
+choice=$(printf "$(date +"%d-%m-%Y_%H:%M").$default_extention\nCopy only\n" | rofi -dmenu -p "Grimshot")
+
+case "$choice" in
+  "$(date +"%d-%m-%Y_%H:%M").$default_extention") name="$(date +"%d-%m-%Y_%H:%M").png" && normalScreenshot;;
+  "Copy only") copy_only ;;
+  *) name=$choice.$default_extention;;
+esac
+
+normalScreenshot
